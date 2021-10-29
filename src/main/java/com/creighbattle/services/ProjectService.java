8d957c8aa602b67dc1383c3ccb3d7c14526c4eda
creigh-battle <creigh.battle@hcl.com>
2021-10-29 12:20:50 -0400

    branch7 - Custom Exceptions

--- src/main/java/com/creighbattle/services/ProjectService.java
+++ src/main/java/com/creighbattle/services/ProjectService.java
@@ -1,6 +1,7 @@
 package com.creighbattle.services;
 
 import com.creighbattle.domain.Project;
+import com.creighbattle.exceptions.ProjectIdException;
 import com.creighbattle.repositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,8 +14,15 @@ public class ProjectService {
 
     public Project saveOrUpdateProject(Project project) {
 
-        //Logic
-        return projectRepository.save(project);
+        try{
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return projectRepository.save(project);
+        } catch (Exception e) {
+            throw new ProjectIdException("Project ID '" + project.getProjectIdentifier().toUpperCase()
+            + "' Already Exists");
+        }
+
+
 
     }
 
