34c597c593695f3ad0ba0d020159734ebeebf2f7
creigh-battle <creigh.battle@hcl.com>
2021-11-10 09:59:53 -0500

    branch14 - create user object

--- src/main/java/com/creighbattle/domain/User.java
+++ src/main/java/com/creighbattle/domain/User.java
@@ -1,4 +1,45 @@
 package com.creighbattle.domain;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import java.util.Date;
+
+@Entity
+@Data
+@NoArgsConstructor
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Email(message = "Username needs to be an email")
+    @NotBlank(message = "Username is required")
+    @Column(unique = true)
+    private String username;
+    @NotBlank(message = "Please enter your full name")
+    private String fullname;
+    @NotBlank(message = "Password field is required")
+    private String password;
+    @Transient
+    private String confirmPassword;
+    private Date created_At;
+    private Date updated_At;
+
+    //OneToMany with project
+
+    @PrePersist
+    protected void onCreate() {
+        this.created_At = new Date();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updated_At = new Date();
+    }
+
 }
