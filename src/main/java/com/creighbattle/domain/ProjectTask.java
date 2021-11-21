4cf20c290d32a51fe21d1cb911e2772ae4972097
creigh-battle <creigh.battle@hcl.com>
2021-11-21 18:48:38 -0500

    branch17 - final course backend

--- src/main/java/com/creighbattle/domain/ProjectTask.java
+++ src/main/java/com/creighbattle/domain/ProjectTask.java
@@ -1,5 +1,6 @@
 package com.creighbattle.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -26,8 +27,11 @@ public class ProjectTask {
     private String acceptanceCriteria;
     private String status;
     private Integer priority;
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date dueDate;
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date created_At;
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date updated_At;
     //ManyToOne with backlog
     @ManyToOne(fetch = FetchType.EAGER)
@@ -38,96 +42,6 @@ public class ProjectTask {
     @Column(updatable = false)
     private String projectIdentifier;
 
-//    public ProjectTask() {
-//    }
-//
-//    public Long getId() {
-//        return id;
-//    }
-//
-//    public void setId(Long id) {
-//        this.id = id;
-//    }
-//
-//    public String getProjectSequence() {
-//        return projectSequence;
-//    }
-//
-//    public void setProjectSequence(String projectSequence) {
-//        this.projectSequence = projectSequence;
-//    }
-//
-//    public String getSummary() {
-//        return summary;
-//    }
-//
-//    public void setSummary(String summary) {
-//        this.summary = summary;
-//    }
-//
-//    public String getAcceptanceCriteria() {
-//        return acceptanceCriteria;
-//    }
-//
-//    public void setAcceptanceCriteria(String acceptanceCriteria) {
-//        this.acceptanceCriteria = acceptanceCriteria;
-//    }
-//
-//    public String getStatus() {
-//        return status;
-//    }
-//
-//    public void setStatus(String status) {
-//        this.status = status;
-//    }
-//
-//    public Integer getPriority() {
-//        return priority;
-//    }
-//
-//    public void setPriority(Integer priority) {
-//        this.priority = priority;
-//    }
-//
-//    public Date getDueDate() {
-//        return dueDate;
-//    }
-//
-//    public void setDueDate(Date dueDate) {
-//        this.dueDate = dueDate;
-//    }
-//
-//    public Date getCreated_At() {
-//        return created_At;
-//    }
-//
-//    public void setCreated_At(Date created_At) {
-//        this.created_At = created_At;
-//    }
-//
-//    public Date getUpdated_At() {
-//        return updated_At;
-//    }
-//
-//    public void setUpdated_At(Date updated_At) {
-//        this.updated_At = updated_At;
-//    }
-//
-//    public String getProjectIdentifier() {
-//        return projectIdentifier;
-//    }
-//
-//    public void setProjectIdentifier(String projectIdentifier) {
-//        this.projectIdentifier = projectIdentifier;
-//    }
-//
-//    public Backlog getBacklog() {
-//        return backlog;
-//    }
-//
-//    public void setBacklog(Backlog backlog) {
-//        this.backlog = backlog;
-//    }
 
     @PrePersist
     protected void onCreate() {
@@ -139,19 +53,5 @@ public class ProjectTask {
         this.updated_At = new Date();
     }
 
-//    @Override
-//    public String toString() {
-//        return "ProjectTask{" +
-//                "id=" + id +
-//                ", projectSequence='" + projectSequence + '\'' +
-//                ", summary='" + summary + '\'' +
-//                ", acceptanceCriteria='" + acceptanceCriteria + '\'' +
-//                ", status='" + status + '\'' +
-//                ", priority=" + priority +
-//                ", dueDate=" + dueDate +
-//                ", created_At=" + created_At +
-//                ", updated_At=" + updated_At +
-//                ", projectIdentifier='" + projectIdentifier + '\'' +
-//                '}';
-//    }
+
 }
